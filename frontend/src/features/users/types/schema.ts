9ea9d42c441143f266ea2cd402766384f6d6fb86
Rabie Menad <rabie.menad@ideine.fr>
2024-07-25 21:13:42 +0200

    ✨ feat - toggle button

--- frontend/src/features/users/types/schema.ts
+++ frontend/src/features/users/types/schema.ts
@@ -4,6 +4,7 @@ export const UsersSchema = z.object({
   name: z.string().min(1, { message: 'Name is required' }),
   email: z.string().email({ message: 'Email is invalid' }),
   states: z.array(z.string()).min(1).max(2),
+  languagesSpoken: z.array(z.string()),
 });
 
 export type UsersFields = z.infer<typeof UsersSchema>;
@@ -12,4 +13,5 @@ export const userDefaultValues: UsersFields = {
   name: '',
   email: '',
   states: [],
+  languagesSpoken: [],
 };
