c1808e4703c60a553c62e8eba25d16d198aecd64
Rabie Menad <rabie.menad@ideine.fr>
2024-07-27 17:09:37 +0200

    ✨ Implement dynamic form with students form

--- frontend/src/features/users/types/schema.ts
+++ frontend/src/features/users/types/schema.ts
@@ -1,21 +1,49 @@
 import { z } from 'zod';
 
-export const UsersSchema = z.object({
-  name: z.string().min(1, { message: 'Name is required' }),
-  email: z.string().email({ message: 'Email is invalid' }),
-  states: z.array(z.string()).min(1).max(2),
-  languagesSpoken: z.array(z.string()),
-  gender: z.string().min(1),
-  skills: z.array(z.string()).max(2),
-  registrationDateAndTime: z.date(),
-  formerEmploymentPeriod: z.array(z.date()).min(2).max(2),
-  salaryRange: z.array(z.number()).min(2).max(2),
-  isTeacher: z.boolean(),
+const StudentSchema = z.object({
+  name: z.string().min(4),
 });
+
+export const UsersSchema = z
+  .intersection(
+    z.object({
+      name: z.string().min(1, { message: 'Name is required' }),
+      email: z.string().email({ message: 'Email is invalid' }),
+      states: z.array(z.string()).min(1).max(2),
+      languagesSpoken: z.array(z.string()),
+      gender: z.string().min(1),
+      skills: z.array(z.string()).max(2),
+      registrationDateAndTime: z.date(),
+      formerEmploymentPeriod: z.array(z.date()).min(2).max(2),
+      salaryRange: z.array(z.number()).min(2).max(2),
+      isTeacher: z.boolean(),
+    }),
+    z.discriminatedUnion('variant', [
+      z.object({
+        variant: z.literal('create'),
+      }),
+      z.object({
+        variant: z.literal('edit'),
+        id: z.string().min(1),
+      }),
+    ])
+  )
+  .and(
+    z.union([
+      z.object({
+        isTeacher: z.literal(true),
+        students: z.array(StudentSchema),
+      }),
+      z.object({
+        isTeacher: z.literal(false),
+      }),
+    ])
+  );
 
 export type UsersFields = z.infer<typeof UsersSchema>;
 
 export const userDefaultValues: UsersFields = {
+  variant: 'create',
   name: '',
   email: '',
   states: [],
@@ -25,5 +53,5 @@ export const userDefaultValues: UsersFields = {
   registrationDateAndTime: new Date(),
   formerEmploymentPeriod: [new Date(), new Date()],
   salaryRange: [0, 5000],
-  isTeacher: true,
+  isTeacher: false,
 };
